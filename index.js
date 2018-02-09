a49437ccdd6bc11929af8873cb864d6af09358b2
Matt Roe <matt.roe@patientpoint.com>
2018-02-09 13:27:05 -0500

    Added readme / created loop to catch all files in src dir

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+// Documentation for excel-as-json
+// https://github.com/stevetarver/excel-as-json
+
+const path = require('path');
+const fs = require('fs');
 const convertExcel = require('excel-as-json').processFile;
+
+const srcFolder = './source/';
+const destFolder = './dest/';
 
 const options = {
   sheet: '1',
@@ -7,10 +15,18 @@ const options = {
 
 const callback = (err) => {
   if (err) {
-    console.log('JSON Conversion Error:' + err);
+    console.log('JSON Conversion Error: ' + err);
+  } else {
+    console.log('JSON Conversion Successful!');
   }
 };
 
-const srcFile = 'source/Data_Test_WRN_bulk.xlsx';
+fs.readdir(srcFolder, (err, files) => {
+  files.forEach((file) => {
+    const srcFile = srcFolder + file;
+    const filePath = 'dest/' + path.parse(file).name;
+    const finalPath = filePath + '/map.json';
 
-convertExcel(srcFile, 'dest/map.json', options, callback);
+    convertExcel(srcFile, finalPath, options, callback);
+  });
+});
