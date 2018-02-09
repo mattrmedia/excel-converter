df6bf5a969cf4e0ee448893b81b355bf6ccf251a
Matt Roe <matt.roe@patientpoint.com>
2018-02-09 12:14:19 -0500

    Added conversion of excel document to json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const convertExcel = require('excel-as-json').processFile;
+
+const options = {
+  sheet: '1',
+  omitEmptyFields: false,
+};
+
+const callback = (err) => {
+  if (err) {
+    console.log('JSON Conversion Error:' + err);
+  }
+};
+
+const srcFile = 'source/Data_Test_WRN_bulk.xlsx';
+
+convertExcel(srcFile, 'dest/map.json', options, callback);
