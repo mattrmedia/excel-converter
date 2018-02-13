5f78ffe1f39db3770516b01d5ab6b008f9eae285
Matt Roe <matt.roe@patientpoint.com>
2018-02-13 17:12:00 -0500

    Added functions to clean the json after conversion

--- index.js
+++ index.js
@@ -11,20 +11,76 @@ const options = {
   omitEmptyFields: false,
 };
 
-const callback = (err) => {
+const flatten = (data) => {
+  const flatArr = data.reduce((obj, value) => {
+    for (const i in value) {
+      if (value[i] !== '') {
+        obj.push(value[i])
+      }
+    }
+    return obj;
+  }, []);
+  return flatArr;
+};
+
+const cleanData = (data) => {
+  const clean = []
+  data.forEach((obj) => {
+    Object.keys(obj).forEach((k) => {
+      if (obj[k] === '' || undefined) {
+        delete obj[k];
+      } else if (Array.isArray(obj[k])) {
+        obj[k] = flatten(obj[k]);
+      }
+    });
+    clean.push(obj);
+  });
+  return clean;
+};
+
+const cb = (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('JSON Cleaned!');
+  }
+}
+
+const getFile = (fpath) => {
+  fs.readFile(fpath, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const newData = JSON.parse(data);
+      const clean = JSON.stringify(cleanData(newData));
+      fs.writeFile(fpath, clean, cb)
+    }
+  });
+};
+
+const cleanFile = (fpath) => {
+  getFile(fpath)
+};
+
+const callback = (err, data, dst) => {
   if (err) {
     console.log('JSON Conversion Error: ' + err);
   } else {
-    console.log('JSON Conversion Successful!');
+    console.log('JSON Conversion Successful!', 'Cleaning JSON.');
+    cleanFile(dst);
   }
 };
 
 fs.readdir(srcFolder, (err, files) => {
-  files.forEach((file) => {
-    const srcFile = srcFolder + file;
-    const filePath = 'dest/' + path.parse(file).name;
-    const finalPath = filePath + '/map.json';
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      const srcFile = srcFolder + file;
+      const filePath = 'dest/' + path.parse(file).name;
+      const finalPath = filePath + '/map.json';
 
-    convertExcel(srcFile, finalPath, options, callback);
-  });
+      convertExcel(srcFile, finalPath, options, callback);
+    });
+  }
 });
